306e206a8804dd6ecabfa4242d293069b88480f0
Ana Luiza <analuizamattos884@gmail.com>
2026-03-15 11:54:40 -0300

    Exercicio 3: complexidade e justificativa

--- src/main/Analise complexidades.py
+++ src/main/Analise complexidades.py
@@ -1,9 +1,18 @@
-def somar_lista(lista):
-    total = 0
-    for elemento in lista: #O(n)
-        total += elemento
-    return total
+def busca_binaria(lista, alvo):
+    esquerda, direita = 0, len(lista) - 1
+
+    while esquerda <= direita:
+        meio = (esquerda + direita) // 2
+
+        if lista[meio] == alvo:
+            return meio
+        elif lista[meio] < alvo:
+            esquerda = meio + 1
+        else:
+            direita = meio - 1
+
+    return -1
 """
-A logica foi que esse algoritmo é linear (O(n)) pois o return está fora do bloco for. 
-Então ele espera o loop percorrer todos os n elementos antes de devolver o resultado
+A logica foi que esse algoritmo é logarítmico (O(log n)) pois ele divide a lista em duas partes a cada iteração.
+Então ele espera o loop percorrer apenas os elementos necessários antes de devolver o resultado
 """
