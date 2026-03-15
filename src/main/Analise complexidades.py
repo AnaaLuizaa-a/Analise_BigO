0f889c281a96f1773358254956e86cb28dcbf9ce
Ana Luiza <analuizamattos884@gmail.com>
2026-03-15 12:16:57 -0300

    Exercicio 6: complexidade e justificativa

--- src/main/Analise complexidades.py
+++ src/main/Analise complexidades.py
@@ -1,10 +1,9 @@
-def imprimir_pares_e_soma(lista);
-    for i in range (len(lista)):
-        print (lista[i])
-    for i in range (len(lista)):
-        for j in range (i+1, len(lista)):
-            print (lista[i], lista[j])
+def potencias_de_dois(n):
+    i = 1
+    while i < n :
+        print(i)
+        i *= 2
 """
-A logica foi que esse algoritmo é O(n^2) porque tem um loop dentro do outro, ou seja, para cada elemento da lista, ele percorre a lista novamente para encontrar os pares. 
-Portanto, o número de operações cresce quadráticamente com o tamanho da lista.
+A logica foi que esse algoritmo é O(log n) porque a cada iteração o valor de i é multiplicado por 2, ou seja, o número de iterações necessárias para alcançar ou ultrapassar n é proporcional ao logaritmo de n na base 2.
+Portanto, o tempo de execução do algoritmo cresce de forma logarítmica em relação ao tamanho da entrada n.
 """
